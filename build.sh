9d6b223133e7edd540d1ecc48f234f7558175fde
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-30 11:39:53 -0700

    Update DockerHub credentials

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ export REGISTRY=$2 # The Docker Registry to use
 export REGISTRY_USERNAME=$3 # The username for the Docker Registry
 export REGISTRY_PASSWORD=$4 # The password for the Docker Registry
 
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+newgrp docker
 cd debian-base/
 #echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin
 docker login -u DOCKER_USERNAME=${REGISTRY_USERNAME} -p DOCKER_PASSWORD=${REGISTRY_PASSWORD}
 #docker build --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME} .
-newgrp docker
 docker build -t ${REGISTRY}/${IMAGE_NAME} .
 docker push ${REGISTRY}/${IMAGE_NAME}
 
