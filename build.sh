ccf9ebe476017c080d709526e8470d96f7b5dce4
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-30 13:58:20 -0700

    Update settings

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export REGISTRY_PASSWORD=$5 # The password for the Docker Registry
 
 cd debian-base/
 #echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin
-docker login docker.io
+#docker login docker.io
 #docker build --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME} .
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
 docker push ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}
