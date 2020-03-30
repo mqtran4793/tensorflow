8acc70c6a21414c508c41ba9cb3b28f417e64787
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-30 14:20:32 -0700

    Fix credentials

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,8 +2,9 @@ pipeline {
    environment {
       //registry = "mqtran4793/repository_name"
       //registryCredential = ‘dockerhub’
-      REGISTRY = "mqtran4793"
-      DOCKER_CONFIG="$HOME/.docker"
+      REGISTRY = "docker.io/mqtran4793"
+      REGISTRY_CREDS = "$HOME/.docker/config.json"
+      //DOCKER_CONFIG="$HOME/.docker"
    }
 
    agent any
@@ -11,7 +12,7 @@ pipeline {
    stages {
       stage('Build debian-base') {
          steps {
-            sh './build.sh debian-base 0.1 ${REGISTRY} '
+            sh './build.sh debian-base 0.1 ${REGISTRY} ${REGISTRY_CREDS_USR} ${REGISTRY_CREDS_PSW}'
          }
       }
    }
--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -e
 export IMAGE_NAME=$1 # The full docker image name (without tag)
 export BRANCH_NAME=$2 # The git branch name (also the resulting docker image tag)
 export REGISTRY=$3 # The Docker Registry to use
-#export REGISTRY_USERNAME=$4 # The username for the Docker Registry
-#export REGISTRY_PASSWORD=$5 # The password for the Docker Registry
+export REGISTRY_USERNAME=$4 # The username for the Docker Registry
+export REGISTRY_PASSWORD=$5 # The password for the Docker Registry
 
 cd debian-base/
-#echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin
+echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin ${REGISTRY}
 #docker login docker.io
 #docker build --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME} .
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
