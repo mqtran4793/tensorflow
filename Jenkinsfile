9715b5c80b6f3fb6bc861006e14bcdd61f3396d1
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-30 14:42:41 -0700

    Fix credentials

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,18 +1,31 @@
 pipeline {
    environment {
-      //registry = "mqtran4793/repository_name"
-      //registryCredential = ‘dockerhub’
-      REGISTRY = "docker.io/mqtran4793"
-      REGISTRY_CREDS = "$HOME/.docker/config.json"
-      DOCKER_CONFIG="$HOME/.docker"
+      registry = "mqtran4793"
+      registryCredential = ‘dockerhub’
+      // REGISTRY = "mqtran4793"
+      // REGISTRY_CREDS = "$HOME/.docker/config.json"
+      // DOCKER_CONFIG="$HOME/.docker"
+      dockerImage = ' '
    }
 
    agent any
 
    stages {
-      stage('Build debian-base') {
-         steps {
-            sh './build.sh debian-base 0.1 ${REGISTRY} ${REGISTRY_CREDS_USR} ${REGISTRY_CREDS_PSW}'
+      stage('Building image') {
+         steps{
+            script {
+               cd debian-base/
+               dockerImage = docker.build registry + "debian-base:$BUILD_NUMBER"
+            }
+         }
+      }
+      stage('Deploy image') {
+         steps{
+            script {
+               docker.withRegistry( '', registryCredential ) {
+                  dockerImage.push()
+               }
+            }
          }
       }
    }
--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ export REGISTRY_USERNAME=$4 # The username for the Docker Registry
 export REGISTRY_PASSWORD=$5 # The password for the Docker Registry
 
 cd debian-base/
-echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin ${REGISTRY}
+#echo ${REGISTRY_PASSWORD} | docker login --username DOCKER_USERNAME=${REGISTRY_USERNAME} --password-stdin ${REGISTRY}
 #docker login docker.io
 #docker build --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME} .
-#docker build -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
-docker build --pull --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
-docker push ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}
+docker build -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
+#docker build --pull --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg DOCKER_USERNAME=${REGISTRY_USERNAME} --build-arg DOCKER_PASSWORD=${REGISTRY_PASSWORD} -t ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME} .
+#docker push ${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}
 
 #docker build -t mqtran4793/test:debian-base .
 #docker push mqtran4793/test:debian-base
