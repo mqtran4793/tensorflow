2a88279f48ac82be4d70ddbd26a3066e93eeb253
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-31 00:28:25 -0700

    BUILD BITCH

--- debian-tf-build/build_all_linux.sh
+++ debian-tf-build/build_all_linux.sh
@@ -20,9 +20,9 @@ set -e
 
 # Make sure we're in the correct directory, at the root of the source tree.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd ${SCRIPT_DIR}/../../
+cd ${SCRIPT_DIR}/../../../
 echo
-pwd
+ehco $HOME
 echo
 source "${SCRIPT_DIR}/build_helper.subr"
 JOB_COUNT="${JOB_COUNT:-$(get_job_count)}"
